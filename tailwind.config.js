f29d7a3c143c66116ec68cf0c8705ce09a402eaa
Amdadul-HQ <rimonamdadul301@gmail.com>
2025-07-03 22:58:05 +0600

    add: helvitica font

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: ['./*.html'],
+  theme: {
+    extend: {
+      fontFamily: {
+        helvetica: ['Helvetica', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+}
